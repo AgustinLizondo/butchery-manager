b8a2e4562dfd7a8e5706791452ac5567d91f6394
Agustin Lizondo <agustinglizondo@gmail.com>
2024-06-22 01:11:37 -0300

    container component modified

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import { View } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 import { IContainerProps } from './types';
+import { twMerge } from 'tailwind-merge';
 
 const Container = (props: IContainerProps) => {
   const {
     children,
+    className,
     ...rest
   } = props;
 
   return (
-    <View
-      className="flex flex-1 rounded-sm p-4 hover:shadow-lg"
+    <TouchableOpacity
+      className={twMerge("flex rounded-sm p-4", className)}
       {...rest}
     >
       {children}
-    </View>
+    </TouchableOpacity>
   )
 }
 
