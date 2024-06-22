455b0bb17db55bd6b9009fd6e076a68a37443f19
Agustin Lizondo <agustinglizondo@gmail.com>
2024-06-22 01:12:23 -0300

    currency formatter & products mock

---
+++ src/utils/ProductsMock.ts
@@ -0,0 +1,52 @@
+export const ProductsMock = [
+  {
+    "id": 0,
+    "name": "Vacío",
+    "price": 3000
+  },
+  {
+    "id": 1,
+    "name": "Lomo de res",
+    "price": 4000
+  },
+  {
+    "id": 2,
+    "name": "Costilla de res",
+    "price": 2500
+  },
+  {
+    "id": 3,
+    "name": "Bistec de res",
+    "price": 3000
+  },
+  {
+    "id": 4,
+    "name": "Entrecot",
+    "price": 3500
+  },
+  {
+    "id": 5,
+    "name": "Filete de res",
+    "price": 3800
+  },
+  {
+    "id": 6,
+    "name": "Falda de res",
+    "price": 2000
+  },
+  {
+    "id": 7,
+    "name": "Solomillo de res",
+    "price": 4500
+  },
+  {
+    "id": 8,
+    "name": "Colita de cuadril",
+    "price": 3200
+  },
+  {
+    "id": 9,
+    "name": "Asado de tira",
+    "price": 2800
+  },
+];
