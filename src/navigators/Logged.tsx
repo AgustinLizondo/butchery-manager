6678f7c320737b2cc8d03e9399f12f3ad334618a
Agustin Lizondo <agustinglizondo@gmail.com>
2024-06-22 01:11:56 -0300

    types for logged navigator

--- src/navigators/Logged.tsx
+++ src/navigators/Logged.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from '../screens/Home';
+import ProductsHomeScreen from '../screens/ProductsHome';
+import { LoggedStackParamList } from './logged.types';
 
 
 const LoggedNavigator = () => {
-  const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator<LoggedStackParamList>();
 
   return (
     <Stack.Navigator
@@ -13,6 +15,7 @@ const LoggedNavigator = () => {
       }}
     >
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="ProductsHome" component={ProductsHomeScreen} />
     </Stack.Navigator>
   )
 }
---
+++ src/navigators/logged.types.ts
@@ -0,0 +1,4 @@
+export type LoggedStackParamList = {
+  Home: undefined;
+  ProductsHome: undefined;
+};
