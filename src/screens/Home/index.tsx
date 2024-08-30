804141bdbab105519d9697268565a9a513d0a5fa
Agustin Lizondo <agustinglizondo@gmail.com>
2024-08-30 01:54:00 -0300

    Homescreen restyling using icons and v0 design

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,7 +3,8 @@ import PageContainer from "../../components/PageContainer";
 import Button from "../../components/Button";
 import { HomeScreenProps } from "./types";
 import { supabase } from "../../utils/supabase";
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 const HomeScreen = (props: HomeScreenProps) => {
   const { navigation } = props;
@@ -13,34 +14,56 @@ const HomeScreen = (props: HomeScreenProps) => {
   return (
     <PageContainer className="flex-col space-y-4">
       <View className="flex flex-1 flex-col space-y-4">
-        <View className="flex flex-1 flex-row flex-wrap space-x-4">
+        <View className="flex flex-1 flex-row space-x-4">
           <Button
-            className="flex flex-1 h-full items-center justify-center rounded-2xl bg-neutral-400"
-            onPress={() => navigation.navigate("Orders")}
+            className="flex flex-1 h-full rounded-2xl items-center justify-center"
+            onPress={() => navigation.navigate("ProductsHome")}
           >
-            Ver órdenes
+            <View className="w-64 h-64 flex-col space-y-4 items-center justify-center">
+              <Feather name="package" size={48} color="white" />
+              <Text className="text-white text-xl">Productos</Text>
+            </View>
           </Button>
           <Button
-            className="flex flex-1 h-full items-center justify-center rounded-2xl bg-neutral-600"
+            className="flex flex-1 h-full items-center justify-center rounded-2xl bg-neutral-200 border border-neutral-300"
             onPress={() => navigation.navigate("Cart")}
           >
-            Ver carrito
+            <View className="w-64 h-64 flex-col space-y-4 items-center justify-center">
+              <Feather name="shopping-cart" size={48} color="black" />
+              <Text className="text-black text-xl">Carrito</Text>
+            </View>
+          </Button>
+        </View>
+        <View className="flex flex-1 flex-row flex-wrap space-x-4">
+          <Button
+            className="flex flex-1 h-full items-center justify-center rounded-2xl bg-neutral-200 border border-neutral-300"
+            onPress={() => navigation.navigate("Orders")}
+          >
+            <View className="w-64 h-64 flex-col space-y-4 items-center justify-center">
+              <Feather name="list" size={48} color="black" />
+              <Text className="text-black text-xl">Órdenes</Text>
+            </View>
+          </Button>
+          <Button
+            className="flex flex-1 h-full items-center justify-center rounded-2xl bg-neutral-200 border border-neutral-300"
+            onPress={onSignOutPress}
+          >
+            <View className="w-64 h-64 flex-col space-y-4 items-center justify-center">
+              <Feather name="log-out" size={48} color="black" />
+              <Text className="text-black text-xl">Cerrar sesión</Text>
+            </View>
           </Button>
         </View>
         <Button
-          className="flex flex-1 h-full items-center justify-center rounded-2xl bg-neutral-900"
-          onPress={() => navigation.navigate("ProductsHome")}
+          className="flex flex-1 h-full items-center justify-center rounded-2xl bg-neutral-200 border border-neutral-300"
+          onPress={() => navigation.navigate("Statistics")}
         >
-          Nuevo pedido
+          <View className="w-64 h-64 flex-col space-y-4 items-center justify-center">
+            <Feather name="bar-chart" size={48} color="black" />
+            <Text className="text-black text-xl">Estadísticas</Text>
+          </View>
         </Button>
       </View>
-      <Button
-        className="bg-transparent p-0"
-        textClassName="text-black"
-        onPress={onSignOutPress}
-      >
-        Cerrar sesión
-      </Button>
     </PageContainer>
   );
 };
